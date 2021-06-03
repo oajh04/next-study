c69ca224ebfddd3a094eaf9ce309c8545e8fbdb0
oajh04 <cloudtubekr@naver.com>
2021-06-03 23:07:25 +0900

    Create : github 레포지토리 클론코팅

---
+++ src/components/GithubRepsitories/UserProfile/styles.ts
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled"
+
+export const UserProfileBox = styled.div`
+    width: 25%;
+    max-width: 272px;
+    margin-right: 26px;
+`;
+
+export const UserProfileImageWrapper = styled.div`
+    width: 100%;
+    border: 1px solid #e1e4e8;
+`;
+
+export const UserProfileImage = styled.img`
+    display: block;
+    border-radius: 50%;
+    width: 100%;
+`
+
+export const UserProfileUserName = styled.h2`
+    margin: 0;
+    padding-top: 16px;
+    font-size: 26px;
+`
+
+export const UserProfileLogin = styled.div`
+    margin: 0;
+    font-size: 20px;
+`
+
+export const UserProfileBio = styled.div`
+    margin: 0;
+    padding-top: 16px;
+    font-size: 14px;
+`
+
+export const UserProfileInfo = styled.div`
+    display: flex;
+    align-self: center;
+    margin: 4px 0 0 ;
+    span {
+        margin-left: 6px;
+    }
+`
+
