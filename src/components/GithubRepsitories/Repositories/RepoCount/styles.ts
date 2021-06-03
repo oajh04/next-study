e5bc89f2c77c7be74cd6b91474ab4fd543191285
oajh04 <cloudtubekr@naver.com>
2021-06-03 23:07:19 +0900

    Create : github 레포지토리 클론코팅

---
+++ src/components/GithubRepsitories/Repositories/RepoCount/styles.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const RepoWrapper = styled.div`
+    
+`
+
+export const RepoHeader = styled.div`
+    padding: 16px 0;
+    font-size: 14px;
+    font-weight: 600;
+    border-bottom: 1px solid #e1e4e8;
+`
+
+export const RepoCount = styled.span`
+    display: inline-block;
+    padding: 2px 5px;
+    margin-left: 6px;
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 1;
+    color: #586069;
+    background-color: rgba(27,31,35,0.08);
+    border-radius: 20px;
+`
